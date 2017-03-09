89bcba8d18d8fb837b3d66550514b7758cd846b9
Florence Mnyazi <florencemnyazi@gmail.com>
2017-03-09 09:56:06 +0300

    Fizz and Buzz test

--- fizzzbuzz.py
+++ fizzzbuzz.py
@@ -7,9 +7,13 @@ def fizz_buzz(x):
  		'''This function returns fizz_buzz when a number x is divisible by both 3 and 5'''
  	elif  x%5 ==0:
  		return "Buzz"
+ 		'''Function returns Buzz if number is divisible by 5'''
+
  	elif  x%3==0:
  		return "Fizz"
+ 		'''Function returns Fizz if number is divisible by 3'''
  	else:
  		return x
+ 		'''Function returns the number if not divisible by either  3, 5 or both 3 nad 5'''
 
-print (fizz_buzz(300))
+print (fizz_buzz())
